8d92741345cdbeb75c2a8497e6d3f803a643ae91
Jean Philippe <bongojeanphilippe@gmail.com>
2023-08-28 14:49:14 +0200

    modif cat

--- resources/views/parametres/produit/categories.blade.php
+++ resources/views/parametres/produit/categories.blade.php
@@ -6,7 +6,8 @@
                     <div class="row mb-2">
                         <div class="col-sm-5">
                             <a href="" class="btn btn-primary mb-2" data-bs-toggle="modal"
-                                data-bs-target="#standard-modal-categorie"><i class="mdi mdi-plus-circle me-2"></i>Nouvelle catégorie</a>
+                                data-bs-target="#standard-modal-categorie"><i
+                                    class="mdi mdi-plus-circle me-2"></i>Nouvelle catégorie</a>
                         </div>
                     </div>
                     <div class="row">
@@ -20,11 +21,11 @@
                             @endif
                             @if ($errors->has('categorie'))
                                 <br>
-                                    <div class="alert alert-warning text-secondary " role="alert">
-                                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert"
-                                            aria-label="Close"></button>
-                                        <strong>{{ $errors->first('categorie') }}</strong>
-                                    </div>
+                                <div class="alert alert-warning text-secondary " role="alert">
+                                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert"
+                                        aria-label="Close"></button>
+                                    <strong>{{ $errors->first('categorie') }}</strong>
+                                </div>
                                 </br>
                             @endif
                         </div>
@@ -40,141 +41,147 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach($categories as $categorie)
-                                    @if($categorie->niveau == 1)
-                                        @include('parametres.produit.categorie', ['categorie' => $categorie])
+                                @foreach ($categories as $categorie)
+                                    @if ($categorie->niveau == 1)
+                                        @include('parametres.produit.categorie', [
+                                            'categorie' => $categorie,
+                                        ])
                                     @endif
                                 @endforeach
                             </tbody>
                         </table>
                     </div>
-                </div> 
-            </div> 
-        </div> 
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
 {{-- Ajout d'une categorie --}}
-    <div id="standard-modal-categorie" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="standard-modalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title" id="standard-modalLabel">Ajouter une catégorie</h4>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <form action="{{ route('categorieproduit.store') }}" method="post">
-                    <div class="modal-body">
-                        @csrf
-                        <div class="col-lg-12">
-                            <div class="form-floating mb-3">
-                                <div class="m-2">
-                                    <label for="inputNom">Nom</label>
-                                    <input type="text" name="nom" class="form-control" id="inputNom" value="">
-                                </div>
-                                <div class="m-2">
-                                    <label for="inputParent">Catégorie parent</label>
-                                    <select name="parent_id" class="form-control" id="inputParent">
-                                        <option value="">Aucune</option>
-                                        @foreach($categories as $categorie)
-                                            @if (!$categorie->parent)
-                                                <optgroup label="{{ hierarchie($categorie) }}">
-                                                    @foreach(allSub($categorie) as $sscategorie)
-                                                        <option value="{{ $sscategorie->id }}">
-                                                            {{ hierarchie($sscategorie) }}
-                                                        </option>
-                                                    @endforeach
-                                                </optgroup>
-                                            @endif
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <div class="m-2">
-                                    <label for="inputDescription">Description</label>
-                                    <textarea name="description" class="form-control" id="inputDescription" rows="4" value=""></textarea>
-                                </div>
-                                @if ($errors->has('categorie'))
-                                    <br>
-                                    <div class="alert alert-warning text-secondary " role="alert">
-                                        <button type="button" class="btn-close btn-close-white"
-                                            data-bs-dismiss="alert" aria-label="Close"></button>
-                                        <strong>{{ $errors->first('categorie') }}</strong>
-                                    </div>
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="submit" class="btn btn-primary">Enregistrer</button>
-                        <button type="button" class="btn btn-light" data-bs-dismiss="modal">Fermer</button>
-
-                    </div>
-                </form>
-
+<div id="standard-modal-categorie" class="modal fade" tabindex="-1" role="dialog"
+    aria-labelledby="standard-modalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="standard-modalLabel">Ajouter une catégorie</h4>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-        </div>
-    </div>
-
-    {{-- Modification d'une categorie --}}
-    <div id="edit-modal-categorie" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="standard-modalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title" id="standard-modalLabel">Modifier la catégorie</h4>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <form action="" method="post" id="form-edit-categorie">
-                    <div class="modal-body">
-                        @csrf
-                        <div class="col-lg-12">
-                            <div class="form-floating mb-3">
+            <form action="{{ route('categorieproduit.store') }}" method="post">
+                <div class="modal-body">
+                    @csrf
+                    <div class="col-lg-12">
+                        <div class="form-floating mb-3">
                             <div class="m-2">
-                                    <label for="inputNomEdit">Nom</label>
-                                    <input type="text" name="nom" class="form-control" id="inputNomEdit" value="">
-                                </div>
-                                <div class="m-2">
-                                    <label for="inputParentEdit">Catégorie parent</label>
-                                    <select name="parent_id" class="form-control" id="inputParentEdit">
-                                        <option value="">Aucune</option>
-                                        @foreach($categories as $categorie)
-                                            @if (!$categorie->parent)
-                                                <optgroup label="{{ $categorie->nom }}">
-                                                    @foreach(allSub($categorie) as $sscategorie)
-                                                        <option value="{{ $sscategorie->id }}">
-                                                            {{ hierarchie($sscategorie) }}
-                                                        </option>
-                                                    @endforeach
-                                                </optgroup>
-                                            @endif
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <div class="m-2">
-                                    <label for="inputDescriptionEdit">Description</label>
-                                    <textarea name="description" class="form-control" id="inputDescriptionEdit" rows="4" value=""></textarea>
-                                </div>
-                                @if ($errors->has('categorie'))
-                                    <br>
-                                    <div class="alert alert-warning text-secondary " role="alert">
-                                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert" aria-label="Close"></button>
-                                        <strong>{{ $errors->first('categorie') }}</strong>
-                                    </div>
-                                @endif
+                                <label for="inputNom">Nom</label>
+                                <input type="text" name="nom" class="form-control" id="inputNom" value="">
                             </div>
+                            <div class="m-2">
+                                <label for="inputParent">Catégorie parent</label>
+                                <select name="parent_id" class="form-control" id="inputParent">
+                                    <option value="">Aucune</option>
+                                    @foreach ($categories as $categorie)
+                                        @if (!$categorie->parent)
+                                            <optgroup label="{{ hierarchie($categorie) }}">
+                                                @foreach (allSub($categorie) as $sscategorie)
+                                                    <option value="{{ $sscategorie->id }}">
+                                                        {{ hierarchie($sscategorie) }}
+                                                    </option>
+                                                @endforeach
+                                            </optgroup>
+                                        @endif
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="m-2">
+                                <label for="inputDescription">Description</label>
+                                <textarea name="description" class="form-control" id="inputDescription" rows="4" value=""></textarea>
+                            </div>
+                            @if ($errors->has('categorie'))
+                                <br>
+                                <div class="alert alert-warning text-secondary " role="alert">
+                                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert"
+                                        aria-label="Close"></button>
+                                    <strong>{{ $errors->first('categorie') }}</strong>
+                                </div>
+                            @endif
                         </div>
                     </div>
-                    <div class="modal-footer">
-                        <button type="submit" class="btn btn-primary">Modifier</button>
-                        <button type="button" class="btn btn-light" data-bs-dismiss="modal">Fermer</button>
-                    </div>
-                </form>
-            </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Enregistrer</button>
+                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">Fermer</button>
+
+                </div>
+            </form>
+
         </div>
     </div>
+</div>
+
+{{-- Modification d'une categorie --}}
+<div id="edit-modal-categorie" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="standard-modalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="standard-modalLabel">Modifier la catégorie</h4>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="" method="post" id="form-edit-categorie">
+                <div class="modal-body">
+                    @csrf
+                    <div class="col-lg-12">
+                        <div class="form-floating mb-3">
+                            <div class="m-2">
+                                <label for="inputNomEdit">Nom</label>
+                                <input type="text" name="nom" class="form-control" id="inputNomEdit"
+                                    value="">
+                            </div>
+                            <div class="m-2">
+                                <label for="inputParentEdit">Catégorie parent</label>
+                                <select name="parent_id" class="form-control" id="inputParentEdit">
+                                    <option value="">Aucune</option>
+                                    @foreach ($categories as $categorie)
+                                        @if (!$categorie->parent)
+                                            <optgroup label="{{ $categorie->nom }}">
+                                                @foreach (allSub($categorie) as $sscategorie)
+                                                    <option value="{{ $sscategorie->id }}">
+                                                        {{ hierarchie($sscategorie) }}
+                                                    </option>
+                                                @endforeach
+                                            </optgroup>
+                                        @endif
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="m-2">
+                                <label for="inputDescriptionEdit">Description</label>
+                                <textarea name="description" class="form-control" id="inputDescriptionEdit" rows="4" value=""></textarea>
+                            </div>
+                            @if ($errors->has('categorie'))
+                                <br>
+                                <div class="alert alert-warning text-secondary " role="alert">
+                                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="alert"
+                                        aria-label="Close"></button>
+                                    <strong>{{ $errors->first('categorie') }}</strong>
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Modifier</button>
+                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">Fermer</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 
 
 <?php
-function hierarchie($categorie) {
+function hierarchie($categorie)
+{
     $hierarchy = $categorie->nom;
 
     $parent = $categorie->parent;
@@ -186,7 +193,8 @@ function hierarchie($categorie) {
     return $hierarchy;
 }
 
-function allSub($categorie) {
+function allSub($categorie)
+{
     $subcategories = collect([$categorie]);
 
     foreach ($categorie->sscategories as $sscategorie) {
@@ -196,7 +204,8 @@ function allSub($categorie) {
     return $subcategories;
 }
 
-function allExcept($categories, $except) {
+function allExcept($categories, $except)
+{
     $filteredCategories = collect();
 
     foreach ($categories as $categorie) {
@@ -217,11 +226,15 @@ function allExcept($categories, $except) {
             let currentParentId = that.data('parent_id');
             let currentDescription = that.data('description');
             let currentFormAction = that.data('href');
-            
+
+            console.log(currentParentId);
+            if (currentParentId)
+                $('#parent_id option[value=' + currentParentId + ']').attr("selected", true);
+
             $('#inputNomEdit').val(currentNom);
             $('#inputParentEdit').val(currentParentId);
             $('#inputDescriptionEdit').val(currentDescription);
-            
+
             $('#form-edit-categorie').attr('action', currentFormAction);
         });
     </script>
