388b7c3dc7c1a1e01f49f9b5920d68c1c5ddf746
Jean Philippe <bongojeanphilippe@gmail.com>
2024-03-25 18:14:22 +0100

    debug ajout et modification de date depense

--- app/Models/depense.php
+++ app/Models/depense.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class depense extends Model
+class Depense extends Model
 {
     use HasFactory;
     protected $guarded =[];
