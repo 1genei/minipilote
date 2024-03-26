4b16e179f0d53068398b3b65d6fb9cc94d13f244
Jean Philippe <bongojeanphilippe@gmail.com>
2024-03-26 10:56:01 +0100

    Modifi nom modèle Depense

---
+++ app/Models/Depense.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Depense extends Model
+{
+    use HasFactory;
+    protected $guarded =[];
+    protected $casts = ['date_depense' => 'date'];
+}
